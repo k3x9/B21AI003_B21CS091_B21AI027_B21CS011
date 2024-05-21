ed4ec123987fc8226875fb289d0cc7a44c3b2d95
k3x9 <97793590+k3x9@users.noreply.github.com>
2024-05-21 12:31:05 +0530

    Delete temp.py

--- temp.py
+++
@@ -1,9 +0,0 @@
-import os
-FILE_NAME = '7'
-FORMAT = '.jpg'
-FILE_NAME_OUT_FOLDER_NAME = 'output_images/' + FILE_NAME
-FILE_NAME, temp = 'input_images/' + FILE_NAME, FILE_NAME
-os.makedirs(FILE_NAME_OUT_FOLDER_NAME, exist_ok=True)
-
-import shutil
-shutil.copy(FILE_NAME + FORMAT, FILE_NAME_OUT_FOLDER_NAME + '/' + temp + FORMAT)
